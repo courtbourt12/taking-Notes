fa0231b304a79855d3105ae4370d19c192340715
courtney long <courtneylong877@gmail.com>
2021-11-19 15:29:01 -0500

    HTML routes are working

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const router = require('express').Router();
+
+
+// GET route for the homepage.
+
+router.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, '../public/index.html'))
+);
+
+// GET route for the note taking page.
+
+router.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "../public/notes.html"))
+);
+
+// Wildcard route to direct users to a 404 page.
+
+router.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '../public/index.html'))
+);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 // Requiring the pages and constructing variables needed to run the server.
 
 const express = require('express');
+// const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const PORT = process.env.port || 3001;
-
 const app = express();
 
 // Middleware for parsing JSON and urlencoded form data.
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+app.use(express.static('public'));
+
 
 app.listen(PORT, () =>
-console.log('App listening at http://localhost:${PORT} 🚀')
+console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
